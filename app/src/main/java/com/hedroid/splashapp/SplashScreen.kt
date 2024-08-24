c11fb236e329d1930154adc4f7fa204289552d7e
Sachin Maurya <kumarsachin67671@gmail.com>
2024-08-24 23:50:02 +0530

    food menu

---
+++ app/src/main/java/com/hedroid/splashapp/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.hedroid.splashapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+         Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+             finish()
+        },1500)
+
+    }
+}
